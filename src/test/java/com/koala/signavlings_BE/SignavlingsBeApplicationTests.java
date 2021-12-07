b70a75d8a8415a5b040ea170a5315ea12db8d0ce
Thorat <preeti.shivaji.thorat@sap.com>
2021-12-07 15:35:07 +0100

    Added Unit test for profile

--- src/test/java/com/koala/signavlings_BE/SignavlingsBeApplicationTests.java
+++ src/test/java/com/koala/signavlings_BE/SignavlingsBeApplicationTests.java
@@ -1,13 +1,39 @@
 package com.koala.signavlings_BE;
 
+import com.koala.signavlings_BE.profileController.ProfileController;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SignavlingsBeApplicationTests {
+    @Test
+    void finaAll() throws JSONException {
+        String actual = "{profileID:1L, name:\"Max Zimmermann\", joiningMonth:\"December\", location:\"Lalaland\", bio:\"I am here for testing\",  role:\"Archangle\" }";
+        JSONAssert.assertEquals(
+                "{profileID:1L, name:\"Max Zimmermann\", joiningMonth:\"December\", location:\"Lalaland\", bio:\"I am here for testing\",  role:\"Archangle\" }", actual, true);
+        JSONAssert.assertEquals("{profileID:1L}", actual, false);
+        JSONAssert.assertEquals("{name:\"Max Zimmermann\"}", actual, false);
+        //JSONAssert.assertEquals("{name:\"Max Zimmermann\"}", actual, true);//This test will fail as the strict checking is true
+        JSONAssert.assertEquals("{joiningMonth:\"December\", bio:\"I am here for testing\", role:\"Archangle\"}", actual, false);
+        //JSONAssert.assertEquals("{name:mkyong}", actual, false);//This test should fail
+    }
 
-	@Test
-	void contextLoads() {
-	}
+
+/*
+
+
+    @Autowired
+    private ProfileController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+       JSONAssert.assertEquals(controller).isNotNull();
+    }
+ */
+
 
 }
